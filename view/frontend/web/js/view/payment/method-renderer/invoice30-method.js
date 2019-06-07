4b2ed91be6bbe941a6ac463792a0f2d1430ee253
Gaurav <gaurav@excellencetechnologies.in>
2019-06-07 19:11:57 +0530

    code formatted

--- view/frontend/web/js/view/payment/method-renderer/invoice30-method.js
+++ view/frontend/web/js/view/payment/method-renderer/invoice30-method.js
@@ -1,22 +1,22 @@
 define(
-[
-'Magento_Checkout/js/view/payment/default'
-],
-function (Component) {
-'use strict';
-return Component.extend({
-defaults: {
-template: 'Excellence_OfflinePaymentMethods/payment/invoice30'
-},
-/** Returns send check to info */
-getMailingAddress: function() {
-return window.checkoutConfig.payment.invoice30.mailingAddress;
-},
-/** Returns payable to info */
-/*getPayableTo: function() {
-return window.checkoutConfig.payment.checkmo.payableTo;
-}*/
-});
-}
+	[
+	'Magento_Checkout/js/view/payment/default'
+	],
+		function (Component) {
+		'use strict';
+		return Component.extend({
+			defaults: {
+			template: 'Excellence_OfflinePaymentMethods/payment/invoice30'
+			},
+			/** Returns send check to info */
+			getMailingAddress: function() {
+			return window.checkoutConfig.payment.invoice30.mailingAddress;
+		},
+		/** Returns payable to info */
+		/*getPayableTo: function() {
+		return window.checkoutConfig.payment.checkmo.payableTo;
+		}*/
+		});
+	}
 );
 
--- view/frontend/web/js/view/payment/offline-payment.js
+++ view/frontend/web/js/view/payment/offline-payment.js
@@ -1,20 +1,18 @@
 define(
-[
-'uiComponent',
-'Magento_Checkout/js/model/payment/renderer-list'
-],
-function (
-Component,
-rendererList
-) {
-'use strict';
-rendererList.push(
-{
-type: 'invoice30',
-component: 'Excellence_OfflinePaymentMethods/js/view/payment/method-renderer/invoice30-method'
-}
-);
-/** Add view logic here if needed */
-return Component.extend({});
-}
+	[
+	'uiComponent',
+	'Magento_Checkout/js/model/payment/renderer-list'
+	],
+		function (
+		Component,
+		rendererList
+		) {
+			'use strict';
+			rendererList.push({
+			type: 'invoice30',
+			component: 'Excellence_OfflinePaymentMethods/js/view/payment/method-renderer/invoice30-method'
+		});
+	/** Add view logic here if needed */
+	return Component.extend({});
+	}
 );
